778b79ebbb71179e794215849ef04ee64e06c7cb
jigyash <realjigyash19@gmail.com>
2026-08-29 12:27:36 +0530

    db connected

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from app.db.database import database
 app=FastAPI()
 @app.get("/")
 def root():
     return {
         "message":"fastapi is running"
+    }
+@app.get("/health")
+async def check_health():
+    await database.command("ping")
+    return{
+        "statues":"ok",
+        "database":"connected"
     }
